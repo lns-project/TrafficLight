2d25607e476baa0c720a641159323a0c19fdc331
Ginza31 <lns-project@yandex.ru>
2023-01-05 12:26:54 +0300

    Add Constants and marks

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,47 +7,68 @@
 
 import UIKit
 
+// MARK: Constants
+
+struct Constants {
+    static let cornerRadius: CGFloat = 10
+    static let disabledAlpha = 0.3
+    static let enabledAlpha = 1.0
+    static let nextLabelButton = "NEXT"
+    static let startLabelButton = "START"
+}
+
+// MARK: ViewController
+
 class ViewController: UIViewController {
 
-    @IBOutlet var actionButton: UIButton!
+    // MARK: Outlets
     
+    @IBOutlet var actionButton: UIButton!
     @IBOutlet var redLightCircle: UIView!
     @IBOutlet var yellowLightCircle: UIView!
     @IBOutlet var greenLightCircle: UIView!
+
+    // MARK: Private properties
     
     private var counter = 0
     
+    // MARK: viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        actionButton.layer.cornerRadius = 10
-        
-        redLightCircle.alpha = 0.3
-        yellowLightCircle.alpha = 0.3
-        greenLightCircle.alpha = 0.3
-        
-        redLightCircle.layer.cornerRadius = 75
-        yellowLightCircle.layer.cornerRadius = 75
-        greenLightCircle.layer.cornerRadius = 75
+        setupUI()
+        disableLights()
     }
     
-
+    // MARK: Methods
+    
     @IBAction func changeColorTap(_ sender: Any) {
-        actionButton.setTitle("NEXT", for: .normal)
-        
-        redLightCircle.alpha = 0.3
-        yellowLightCircle.alpha = 0.3
-        greenLightCircle.alpha = 0.3
+        actionButton.setTitle(Constants.nextLabelButton, for: .normal)
+        disableLights()
         
         switch counter % 3 {
-        case 0: redLightCircle.alpha = 1.0
-        case 1: yellowLightCircle.alpha = 1.0
-        case 2: greenLightCircle.alpha = 1.0
+        case 0: redLightCircle.alpha = Constants.enabledAlpha
+        case 1: yellowLightCircle.alpha = Constants.enabledAlpha
+        case 2: greenLightCircle.alpha = Constants.enabledAlpha
         default:
             break
         }
         counter = counter + 1
-
-        
+    }
+    
+    // MARK: Private methods
+    
+    private func setupUI() {
+        actionButton.setTitle(Constants.startLabelButton, for: .normal)
+        actionButton.layer.cornerRadius = Constants.cornerRadius
+        redLightCircle.layer.cornerRadius = redLightCircle.frame.width/2
+        yellowLightCircle.layer.cornerRadius = yellowLightCircle.frame.width/2
+        greenLightCircle.layer.cornerRadius = greenLightCircle.frame.width/2
+    }
+    
+    private func disableLights() {
+        redLightCircle.alpha = Constants.disabledAlpha
+        yellowLightCircle.alpha = Constants.disabledAlpha
+        greenLightCircle.alpha = Constants.disabledAlpha
     }
 }
-
